21c0b969ab67c1698b761020c1ca29a8a6c986a1
Mi6aRdnv <95938371+Mi6aRdnv@users.noreply.github.com>
2022-02-27 18:09:46 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,25 +16,24 @@ document.querySelectorAll(".tabs-triggers__item").forEach((item) =>
     document.getElementById(id).classList.add("tabs-content__item--active");
   })
 );
+
 document.querySelector(".tabs-triggers__item").click();
 
+let cards = document.querySelectorAll(".pricing-card");
+let selfCard = document.querySelectorAll(".card-top");
 
+for (let i = 0; i < cards.length; i++) {
+  let hover = cards[i];
+  let selfHover = selfCard[i];
 
-let cards = document.querySelectorAll('.pricing-card');
-let selfCard = document.querySelectorAll('.card-top');
-for(let i=0; i < cards.length; i++){
-    let hover = cards[i];
-    let selfHover = selfCard[i];
-    if(!(i == 1)){
-        hover.onmouseenter = function () {
-            selfHover.classList.add('zero');
-            selfCard[1].classList.remove('zero');
-        }
-        
-        hover.onmouseleave = function () {
-            selfHover.classList.remove('zero');
-            selfCard[1].classList.add('zero');
-        }
-    }
-    
+  if (!(i == 1)) {
+    hover.onmouseenter = function () {
+      selfHover.classList.add("zero");
+      selfCard[1].classList.remove("zero");
+    };
+    hover.onmouseleave = function () {
+      selfHover.classList.remove("zero");
+      selfCard[1].classList.add("zero");
+    };
+  }
 }
